8efc1af6b5a9821a960d4954cf42882c4099a911
Mohsen (M.) Zainalpour Aghbolaghi <mohsen.zainalpour.aghbolaghi@ing.com>
2020-06-20 12:30:20 +0200

    Implement a Http client for Hashing-as-a-service API

---
+++ src/main/scala/com/ing/hashservice/cli/client/HashingService.scala
@@ -0,0 +1,43 @@
+package com.ing.hashservice.cli.client
+
+import com.ing.hashservice.cli.config.ServiceConfig
+import io.circe.generic.auto._
+import sttp.client.asynchttpclient.zio.SttpClient
+import sttp.client.circe._
+import sttp.client.{basicRequest, _}
+import zio.{Has, ZIO, ZLayer}
+
+object HashingService {
+  final case class HashRequest(id: String, lines: List[String])
+
+  final case class HashResponse(id: String, lines: List[String])
+
+  type HashingService = Has[HashingService.Service]
+
+  trait Service {
+    def send(request: HashRequest): ZIO[SttpClient, Throwable, HashResponse]
+  }
+
+  final class Live(config: ServiceConfig) extends Service {
+    private val baseUrl = s"${config.scheme}://${config.host}:${config.port}"
+
+    override def send(hashRequest: HashRequest): ZIO[SttpClient, Throwable, HashResponse] = {
+      val request = basicRequest.post(uri"$baseUrl/api/service").body(hashRequest).response(asJson[HashResponse])
+      SttpClient
+        .send(request)
+        .foldM(
+          e => ZIO.fail(e),
+          _.body.fold(ZIO.fail(_), ZIO.succeed(_))
+        )
+    }
+  }
+
+  def live: ZLayer[Has[ServiceConfig] with SttpClient, Throwable, HashingService] =
+    ZLayer.fromFunction { env =>
+      val config = env.get
+      new Live(config)
+    }
+
+  def send(hashRequest: HashRequest): ZIO[HashingService with SttpClient, Throwable, HashResponse] =
+    ZIO.accessM(_.get.send(hashRequest))
+}
